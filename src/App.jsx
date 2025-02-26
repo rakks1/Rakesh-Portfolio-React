eb9a3d9e56df002369e097d3f4a081607b082dff
Rakesh Kumar S <rakeshkumarneelankarai@gmail.com>
2025-02-26 17:36:45 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
 import Home from './Component/Home';
 import About from './Component/About';
